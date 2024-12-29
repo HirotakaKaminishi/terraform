8eb5c029621063da1fa7c2c87231b363df019c41
HirotakaKaminishi  <cb09020+git@gmail.com>
2024-12-29 16:26:12 +0900

    Initial commit

--- enviroments/dev/main.tf
+++ enviroments/dev/main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   vpc_cidr             = "10.0.0.0/16"
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones   = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  availability_zones   = ["ap-northeast-1a", "ap-northeast-1c"]
 }
 
 module "ecs" {
@@ -26,7 +26,7 @@ module "ecs" {
   image_version        = module.ecr.image_version
   repository_url       = module.ecr.repository_url
   container_port       = 80
-  desired_count        = 0
+  desired_count        = 1
   force_new_deployment = true
   subnet_ids           = module.vpc.public_subnet_ids
   tags = {
@@ -45,7 +45,7 @@ module "alb" {
 
   project_name   = "my-ecs-project"
   vpc_id         = module.vpc.vpc_id
-  subnet_ids     = module.vpc.private_subnet_ids
+  subnet_ids     = module.vpc.public_subnet_ids
   container_port = 80
   tags = {
     "Project" = "my-ecs-project"
