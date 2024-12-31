5f721e5c74548c1e866a8df4af9b4d3ac916e0c3
HirotakaKaminishi  <cb09020+git@gmail.com>
2024-12-31 15:07:59 +0900

    Initial commit

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -46,3 +46,11 @@ output "db_password" {
   value       = aws_db_instance.main.password
   sensitive   = true
 }
+
+output "db_instance_endpoint" {
+  value = aws_db_instance.main.endpoint
+}
+
+output "db_instance_arn" {
+  value = aws_db_instance.main.arn
+}
